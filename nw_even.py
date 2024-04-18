fd8bb3c30cc79c4028176ae08441bddab675a046
Rahulr2101 <rahulrajesh2101@gmail.com>
2024-04-18 22:57:01 +0530

    Fix: Corrected minor typos for better code readability

--- nw_even.py
+++ nw_even.py
@@ -18,15 +18,18 @@ def switch(env,es,speed):
             yield sw1.put(packet)
         else:
             yield sw2.put(packet)
-
-def send_packet_dst(env,sw,speed):
+        
+def send_packet_to_es3(env,es,sw,speed):
+    '''
+    Sends packets received by the switch to es3.
+    '''
     while True:
         packet = yield sw.get()
         packet_size = 1000
         transmission_delay = packet_size/speed
         print(f"Transmitting packet{packet.id} from {packet.src} to es3 - Delay: {transmission_delay:.4f} seconds - Current time:{env.now} seconds")
         yield env.timeout(transmission_delay)
-        sw.get()
+        yield es.put(packet)
 
 def packet_generator(env,src,dst,host):
     packet_id = 1
@@ -35,6 +38,7 @@ def packet_generator(env,src,dst,host):
         packet = Packet(packet_id,src,dst)
         yield host.put(packet)
         packet_id += 1
+
 env = simpy.Environment()
 
 es1 = simpy.Store(env,capacity=10)
@@ -43,21 +47,24 @@ es3 = simpy.Store(env,capacity=10)
 sw1 = simpy.Store(env,capacity=10)
 sw2 = simpy.Store(env,capacity=10)
 
+'''
+Calling packet Generator to fill es1 with packets
+'''
 host_process1 = env.process(packet_generator(env,"es1","switch1", es1))
 host_process2 = env.process(packet_generator(env,"es2","switch2",es2))
 
-
-link_speed1 = 100  # 100 Mbps
-link_speed2 = 200  # 500 Mbps
-link_speed3 = 400  # 600 Mbps
-link_speed4 = 200  # 1.5 Gbps
+link_speed1 = 100  
+link_speed2 = 200 
+link_speed3 = 400  
+link_speed4 = 200  
 link_speed5 = 500
 
 switch_process1 = env.process(switch(env, es1, link_speed1))
-env.process(send_packet_dst(env,sw1,link_speed2))
+env.process(send_packet_to_es3(env,es3,sw1,link_speed2))
 switch_process2 = env.process(switch(env, es2, link_speed3))
-env.process(send_packet_dst(env,sw2,link_speed5))
+env.process(send_packet_to_es3(env,es3,sw2,link_speed5))
 
-env.run(until=20) 
+env.run(until=100) 
+
 
 
