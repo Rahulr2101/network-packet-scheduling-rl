a29f22ff3a0fb2f4f6ac71ffcd53eb3b207c07bb
Rahulr2101 <rahulrajesh2101@gmail.com>
2024-04-19 22:28:07 +0530

    fix:Converted network environment functions into methods of a class.

---
+++ algo.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from nw_even import NetworkEnvironment
+
+
+
+
+env = NetworkEnvironment(1)
--- nw_even.py
+++ nw_even.py
@@ -3,68 +3,79 @@ import simpy
 
 packet_size = 1000
 
+
 class Packet:
-    def __init__(self,id,src,dst):
+    def __init__(self, id, src, dst):
         self.id = id
         self.src = src
         self.dst = dst
 
-def switch(env,es,speed):
-    
-    while True:
-        packet = yield es.get()
-        transmission_delay = packet_size / speed
-        print(f"Transmitting packet{packet.id} from {packet.src} to {packet.dst} - Delay: {transmission_delay:.4f} seconds  - Current time:{env.now} seconds - Current Switch 1 Queue Length: {len(sw1.items)}') - Current Switch 2 Queue Length: {len(sw2.items)} ")
-        packet.src = packet.dst
-        yield env.timeout(transmission_delay)
-        if packet.dst == "switch1":
-            yield sw1.put(packet)
-        else:
-            yield sw2.put(packet)
-        
-def send_packet_to_es3(env,es,sw,speed):
-    '''
-    Sends packets received by the switch to es3.
-    '''
-    while True:
-        packet = yield sw.get()
-        transmission_delay = packet_size/speed
-        print(f"Transmitting packet{packet.id} from {packet.src} to es3 - Delay: {transmission_delay:.4f} seconds - Current time:{env.now} seconds")
-        yield env.timeout(transmission_delay)
-        yield es.put(packet)
-
-def packet_generator(env,src,dst,host):
-    packet_id = 1
-    while True:
-        
-        packet = Packet(packet_id,src,dst)
-        yield host.put(packet)
-        packet_id += 1
-
-env = simpy.Environment()
-
-es1 = simpy.Store(env,capacity=10)
-es2 = simpy.Store(env,capacity=10)
-es3 = simpy.Store(env,capacity=10)
-sw1 = simpy.Store(env,capacity=10)
-sw2 = simpy.Store(env,capacity=10)
-
-'''
-Generates packets with increasing IDs at a specified interval
-'''
-host_process1 = env.process(packet_generator(env,"es1","switch1", es1))
-host_process2 = env.process(packet_generator(env,"es2","switch2",es2))
-
-link_speed1 = 1000  
-link_speed2 = 300 
-link_speed3 = 400  
-link_speed4 = 800 
-link_speed5 = 200
-
-switch_process1 = env.process(switch(env, es1, link_speed1))
-env.process(send_packet_to_es3(env,es3,sw1,link_speed2))
-switch_process2 = env.process(switch(env, es2, link_speed3))
-env.process(send_packet_to_es3(env,es3,sw2,link_speed5))
-env.run(until=100) 
 
 
+
+
+class NetworkEnvironment:
+    """
+    Represents the network environment for reinforcement learning.
+    """
+
+    def __init__(self, time,capacity=10, link_speeds=[1000, 300, 400, 800, 200]):
+        self.env = simpy.Environment()
+        self.es1 = simpy.Store(self.env, capacity=capacity)
+        self.es2 = simpy.Store(self.env, capacity=capacity)
+        self.es3 = simpy.Store(self.env, capacity=capacity)
+        self.sw1 = simpy.Store(self.env, capacity=capacity)
+        self.sw2 = simpy.Store(self.env, capacity=capacity)
+        self.link_speeds = link_speeds
+
+        self.create_environment(time)
+          # Call the environment setup function
+
+
+    def switch(self,env, es, speed):
+        """
+        Simulates packet switching behavior.
+        """
+        while True:
+            packet = yield es.get()
+            transmission_delay = packet_size / speed
+            print(f"Transmitting packet{packet.id} from {packet.src} to {packet.dst} - Delay: {transmission_delay:.4f} seconds  - Current time:{env.now} seconds - Current Switch 1 Queue Length: {len(self.sw1.items)}') - Current Switch 2 Queue Length: {len(self.sw2.items)} ")
+            packet.src = packet.dst
+            yield env.timeout(transmission_delay)
+            if packet.dst == "switch1":
+                yield self.sw1.put(packet)
+            else:
+                yield self.sw2.put(packet)
+
+    def send_packet_to_es3(self, env, es, sw, speed):
+        """
+        Sends packets received by the switch to es3.
+        """
+        while True:
+            packet = yield sw.get()
+            transmission_delay = packet_size / speed
+            print(f"Transmitting packet{packet.id} from {packet.src} to es3 - Delay: {transmission_delay:.4f} seconds - Current time:{env.now} seconds")
+            yield env.timeout(transmission_delay)
+            yield es.put(packet)
+
+    def packet_generator(self, env, src, dst, host):
+        """
+        Generates packets with increasing IDs at a specified interval.
+        """
+        packet_id = 1
+        while True:
+            packet = Packet(packet_id, src, dst)
+            yield host.put(packet)
+            packet_id += 1
+
+    def create_environment(self,time):
+        """
+        Sets up the network environment with processes.
+        """
+        host_process1 = self.env.process(self.packet_generator(self.env, "es1", "switch1", self.es1))
+        host_process2 = self.env.process(self.packet_generator(self.env, "es2", "switch2", self.es2))
+        switch_process1 = self.env.process(self.switch(self.env, self.es1, self.link_speeds[0]))
+        self.env.process(self.send_packet_to_es3(self.env, self.es3,self.sw1,self.link_speeds[1]))
+        switch_process2 = self.env.process(self.switch(self.env,self.es2,self.link_speeds[2]))
+        self.env.process(self.send_packet_to_es3(self.env,self.es3,self.sw2,self.link_speeds[4]))
+        self.env.run(until=time)
