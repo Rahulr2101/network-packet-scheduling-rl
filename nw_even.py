a2811fa093957ecf7e89edd5f5d48b48e111a027
Rahulr2101 <rahulrajesh2101@gmail.com>
2024-04-25 20:10:37 +0530

    Add: Tabulate for better output

--- nw_even.py
+++ nw_even.py
@@ -23,9 +23,8 @@ class NetworkEnvironment:
         self.sw2 = simpy.Store(self.env, capacity=self.max_capacity )
         self.actions_step ={0:"sw1",1:"sw2"}
         self.link_speeds = link_speeds
-
+        self.logs_list = []
         self.create_environment(time)
-          # Call the environment setup function
 
 
     def switch(self,es,sw,speed):
@@ -35,14 +34,10 @@ class NetworkEnvironment:
         while True:
             packet = yield es.get()
             transmission_delay = packet_size / speed
-            print(f"Transmitting packet{packet.id} from {packet.src} to {packet.dst} - Delay: {transmission_delay:.4f} seconds  - Current time:{self.env.now} seconds - Current Switch 1 Queue Length: {len(self.sw1.items)}' - Current Switch 2 Queue Length: {len(self.sw2.items)} ")
+            self.logs_list.append([packet.id,packet.src +" to " + packet.dst,transmission_delay,self.env.now,len(self.sw1.items),len(self.sw2.items) ])
             packet.src = packet.dst
             yield self.env.timeout(transmission_delay)
             yield sw.put(packet)
-            # if packet.dst == "switch1":
-            #     yield self.sw1.put(packet)
-            # else:
-            #     yield self.sw2.put(packet)
 
             
     def send_packet_to_sw(self,from_sw, to_sw):
@@ -68,31 +63,27 @@ class NetworkEnvironment:
         while True:
             packet = yield sw.get()
             transmission_delay = packet_size / speed
-            print(f"Transmitting packet{packet.id} from {packet.src} to es3 - Delay: {transmission_delay:.4f} seconds - Current time:{env.now} seconds")
+            packet.dst = "es3"
+            self.logs_list.append([packet.id,packet.src +" to " + "es3",transmission_delay,self.env.now,len(self.sw1.items),len(self.sw2.items) ])
             yield env.timeout(transmission_delay)
             yield es.put(packet)
 
-    def packet_generator(self, src, dst, host, packet_number=5, starting_id=1):
-        packet_id = starting_id
+    def packet_generator(self, src, dst, host, packet_number=5):
         while packet_number > 0:
             packet = Packet(uuid.uuid4(),src, dst)
             yield host.put(packet)
-            
             packet_number -= 1
 
             
-    def display(input = None):
-        if input == None:
-            print(tabulate.tabulate(headers=["ID","Action","Delay","Current_time","Switch 1 Queue","Switch 2 Queue"]))
-        else:
-            print(tabulate.tabulate(input))
+    def display(self):
+        print(tabulate.tabulate(self.logs_list, headers=["Packet ID", "Action", "Delay(in Sec)", "Current_time(in Sec)", "Switch 1 Queue Length", "Switch 2 Queue Length"]))
+
 
     
     def create_environment(self,time):
         """
         Sets up the network environment with processes.
         """
-        self.display()
         host_process1 = self.env.process(self.packet_generator( "es1", "switch1", self.es1))
         host_process2 = self.env.process(self.packet_generator( "es2", "switch2", self.es2))
         switch_process1 = self.env.process(self.switch( self.es1, self.sw1,self.link_speeds["sw1"]["es1"]))
@@ -100,3 +91,4 @@ class NetworkEnvironment:
         switch_process2 = self.env.process(self.switch(self.es2,self.sw2,self.link_speeds["sw2"]["es2"]))
         self.env.process(self.send_packet_to_es3(self.env,self.es3,self.sw2,self.link_speeds["sw1"]["dest"]))
         self.env.run(until=time)
+        self.display()
